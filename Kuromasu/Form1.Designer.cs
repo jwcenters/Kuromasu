df2b0a1ba8eadb5c73f153aaa633fe8ef2a34239
centersjw <centersjw@PRECISION-CENTE>
2017-11-08 11:36:05 -0500

    new project

--- Kuromasu/Form1.Designer.cs
+++ Kuromasu/Form1.Designer.cs
@@ -28,9 +28,17 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
+      this.SuspendLayout();
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.Text = "Form1";
+      this.ClientSize = new System.Drawing.Size(1279, 560);
+      this.Name = "Form1";
+      this.Text = "Kuromasu";
+      this.ResumeLayout(false);
+
     }
 
     #endregion
